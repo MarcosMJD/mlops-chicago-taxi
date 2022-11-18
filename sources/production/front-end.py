d7ee776893af45c62b3ee80aefec84a48d581dfb
Ubuntu <ubuntu@ip-172-31-27-7.eu-west-1.compute.internal>
2022-11-18 09:40:22 +0000

    updated fron-end

--- sources/production/front-end.py
+++ sources/production/front-end.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import os
 import uuid
+import base64
 
 
 def render(api_gateway_base_url, mlflow_external_ip, prefect_external_ip):
@@ -90,6 +91,23 @@ def render(api_gateway_base_url, mlflow_external_ip, prefect_external_ip):
         '9',
     ]
 
+    import subprocess
+    # def run_and_display_stdout(*cmd_with_args):
+    
+
+    def run_bath_monitor():
+        """ 
+        result = subprocess.Popen(("python", "./monitoring/batch/batch_monitoring.py"), stdout=subprocess.PIPE)
+        for line in iter(lambda: result.stdout.readline(), b""):
+            st.text(line.decode("utf-8"))
+        """
+        command = "python ./monitoring/batch/batch_monitoring.py"
+        os.system(command)
+
+    def run_flow():
+        command = "prefect deployment run main-flow/chicago-taxi-deployment"
+        os.system(command)
+
     st.markdown(
         f"MLFlow Experiment Tracking URI: [Click to open](http://{mlflow_external_ip}:8080)",
         unsafe_allow_html=True,
@@ -98,6 +116,12 @@ def render(api_gateway_base_url, mlflow_external_ip, prefect_external_ip):
         f"Prefect Workflow Orchestration URI: [Click to open](http://{prefect_external_ip}:8080/runs)",
         unsafe_allow_html=True,
     )
+    st.button("Execute trainning pipeline", help="Schedule the tranning flow on Prefect server, Prefect agent will actually execute the trainning and model registration", on_click=run_flow)
+    st.markdown("After clicking, go to the Prefect server URI and check the running flow.")
+    st.markdown("After the flow finishes, go to MLflow uri and check the experiments and registered models.")
+
+    st.button("Execute batch monitor", help="Run batch monitor with reference data and actual predicted data. After finishing, you can click to open the report.", on_click=run_bath_monitor)
+    
 
     api_uri = st.text_input(
         "API Endpoint URI",
@@ -125,8 +149,8 @@ def render(api_gateway_base_url, mlflow_external_ip, prefect_external_ip):
 
     st.header(f"Predicted trip time = {prediction:02f} minutes")
 
-    st.image("../../assets/chicago-taxi-infrastructure.jpg")
-    st.image("../../assets/ml-lifecycle-mlops-eternal-knot.png")
+    st.image("../assets/chicago-taxi-infrastructure.jpg")
+    st.image("../assets/ml-lifecycle-mlops-eternal-knot.png")
 
 
 if __name__ == "__main__":
